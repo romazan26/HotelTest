7bdb2e7dca5b79721b2863d4f711ebc8cc16be43
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-18 22:53:44 +0300

    first screen

--- HotelTest/HotelView.swift
+++ HotelTest/HotelView.swift
@@ -7,18 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HotelView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HotelView()
 }
