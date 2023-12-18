381a9060b3586e89a69263710e9ae8b694bcc3c0
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-18 21:53:19 +0300

    Initial Commit

---
+++ HotelTest/HotelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelTestApp.swift
+//  HotelTest
+//
+//  Created by Роман on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
