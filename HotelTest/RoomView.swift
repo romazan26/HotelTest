4306be3c8573d9ab62bd6436f6e72c363726ef9e
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-22 11:22:02 +0300

    add room view

---
+++ HotelTest/RoomView.swift
@@ -0,0 +1,18 @@
+//
+//  RoomView.swift
+//  HotelTest
+//
+//  Created by Роман on 22.12.2023.
+//
+
+import SwiftUI
+
+struct RoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomView()
+}
