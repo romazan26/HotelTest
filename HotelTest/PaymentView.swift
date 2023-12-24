cfd858c893bc12a870d1d2f246a608c058bb2f1f
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-24 12:37:58 +0300

    add Payment view

---
+++ HotelTest/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  HotelTest
+//
+//  Created by Роман on 24.12.2023.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
